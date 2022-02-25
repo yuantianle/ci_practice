7c8aca0a2d66cd92e131848cfa7ddc02f6524208
Tianle Yuan <61530469+yuantianle@users.noreply.github.com>
2022-02-24 23:02:18 -0800

    Function1_int&float_Fix&Tests

--- task.py
+++ task.py
@@ -1,2 +1,52 @@
-def my_func():
-    return "Hello World"
+import string
+
+hexset = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'A', 'B', 'C', 'D', 'E', 'F'
+          'a', 'b', 'c', 'd', 'e', 'f']
+
+def conv_num(num_str):
+    minus_flag = False
+    if num_str[0] == '-':
+        num_str = num_str[1:]
+        minus_flag = True
+
+    float_flag = 0
+    point_index = -1
+
+    if string.isdigit(num_str):
+        sum = 0
+        len = len(num_str)
+        for i in range(len(num_str)):
+            sum += pow(10, (len - i))
+        if minus_flag:
+            return -sum
+        else:
+            return sum
+
+    if (num_str[0] == '0') and (num_str[1] == 'x'):
+        for i in range(len(num_str)):
+            if i != 0 and i != 1 and num_str[i] not in hexset:
+                return None
+        return None
+    else:
+        for i in range(len(num_str)):
+            if num_str[i] in '~`!@#$%^&*()_+-=':
+                return None
+            elif num_str[i] == ".":
+                if float_flag < 1:
+                    float_flag += 1
+                    point_index = i
+                else:
+                    return None
+        sum = 0.0
+        Positionflag = 0
+        for i in range(point_index, 0, -1):
+            sum += (num_str[i]-'0')*pow(10, Positionflag)
+            Positionflag += 1
+        Positionflag = 1
+        for i in range(point_index+1, len(num_str)):
+            sum += (num_str[i]-'0')*pow(10, -Positionflag)
+            Positionflag += 1
+        if minus_flag:
+            return -sum
+        else:
+            return sum
--- tests.py
+++ tests.py
@@ -1,11 +1,17 @@
 import unittest
-
+from task import conv_num
 
 class TestCase(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    def test_1(self):
+        input = "12345"
+        expection = 12345
+        self.assertEqual(conv_num(input), expection)
 
+    def test_1(self):
+        input = "-1234.5"
+        expection = -1234.5
+        self.assertEqual(conv_num(input), expection)
 
 if __name__ == '__main__':
     unittest.main()
