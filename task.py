33488cd8937c91eef29477c1feb1a3b2c0430fce
Tianle Yuan <61530469+yuantianle@users.noreply.github.com>
2022-02-25 13:05:58 -0800

    Function1_encapsulation

--- task.py
+++ task.py
@@ -1,55 +1,62 @@
 # import string
 
+def f1_transfer_digit(num_str, minus_flag):
+    llen = len(num_str)
+    sum = 0
+    for i in range(llen):
+        sum += int(num_str[i]) * pow(10, (llen - i - 1))
+    if minus_flag:
+        return -sum
+    else:
+        return sum
 
-def conv_num(num_str):
+def f1_transfer_float(num_str, minus_flag):
+    float_flag = 0
+    point_index = -1
+    for i in range(len(num_str)):
+        if num_str[i] in '~`!@#$%^&*()_+-=':
+            return None
+        elif num_str[i] == ".":
+            if float_flag < 1:
+                float_flag += 1
+                point_index = i
+            else:
+                return None
+    sum = 0.0
+    Positionflag = 0
+    for i in range(point_index - 1, -1, -1):
+        sum += (int(num_str[i])) * pow(10, Positionflag)
+        Positionflag += 1
+    Positionflag = 1
+    for i in range(point_index + 1, len(num_str)):
+        sum += (int(num_str[i])) * pow(10, -Positionflag)
+        Positionflag += 1
+    if minus_flag:
+        return -sum
+    else:
+        return sum
+
+def f1_transfer_hex(num_str, minus_flag):
     hexset = ['0', '1', '2', '3', '4', '5', '6',
               '7', '8', '9', '10', 'A', 'B', 'C',
               'D', 'E', 'F', 'a', 'b', 'c', 'd', 'e', 'f']
 
+    for i in range(len(num_str)):
+        if i != 0 and i != 1 and num_str[i] not in hexset:
+            return None
+    return None
+
+def conv_num(num_str):
+    
     minus_flag = False
     if num_str[0] == '-':
         num_str = num_str[1:]
         minus_flag = True
 
-    float_flag = 0
-    point_index = -1
-
-    llen = len(num_str)
     if num_str.isdigit():
-        sum = 0
-
-        for i in range(llen):
-            sum += int(num_str[i]) * pow(10, (llen - i - 1))
-        if minus_flag:
-            return -sum
-        else:
-            return sum
+        f1_transfer_digit(num_str, minus_flag)
 
     if (num_str[0] == '0') and (num_str[1] == 'x'):
-        for i in range(len(num_str)):
-            if i != 0 and i != 1 and num_str[i] not in hexset:
-                return None
-        return None
+        f1_transfer_float(num_str, minus_flag)
     else:
-        for i in range(len(num_str)):
-            if num_str[i] in '~`!@#$%^&*()_+-=':
-                return None
-            elif num_str[i] == ".":
-                if float_flag < 1:
-                    float_flag += 1
-                    point_index = i
-                else:
-                    return None
-        sum = 0.0
-        Positionflag = 0
-        for i in range(point_index-1, -1, -1):
-            sum += (int(num_str[i])) * pow(10, Positionflag)
-            Positionflag += 1
-        Positionflag = 1
-        for i in range(point_index+1, len(num_str)):
-            sum += (int(num_str[i])) * pow(10, -Positionflag)
-            Positionflag += 1
-        if minus_flag:
-            return -sum
-        else:
-            return sum
+        f1_transfer_hex(num_str, minus_flag)
